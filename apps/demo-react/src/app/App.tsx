b6c5037dfb8d96c7ae279114fb715cabe8486bbf
Anh Tuan <hauduedaiduong@gmail.com>
2023-05-12 15:29:44 +0700

    Add demo-react app

---
+++ apps/demo-react/src/app/App.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './App.module.scss';
+
+export function App() {
+  return (
+    <div>
+      <h1>
+        <span> Hello there, </span>
+        Welcome demo-react 👋
+      </h1>
+    </div>
+  );
+}
+
+export default App;
